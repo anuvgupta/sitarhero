81c0fb92bfc9dbd653591a7797345e8e92640047
Anuv Gupta <anuv.ca@gmail.com>
2016-09-18 16:56:17 -0700

    fix opacity

--- app.js
+++ app.js
@@ -17,49 +17,49 @@ sitarhero
         })
     )
     .add('div', 'content')
-    .load(function (block) {
-        block.fill(document.body);
-    }, 'app', 'jQuery')
 ;
 
 // display window
 $(document).ready(function () {
-    var content = sitarhero.child('content');
-    // load markdown from github readme
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/anuvgupta/sitarhero/master/README.md',
-        success: function (data) {
-            // resize window handler
-            $(window).resize(function () {
-                var screenshot = $("img[alt='Sitar Hero Screenshot']");
-                if (window.innerWidth < 800)
-                    content.css({
-                        width: '92.6%',
-                        borderRadius: '5px',
-                        margin: '40px auto 0 auto'
+    sitarhero.load(function () {
+        var content = sitarhero.child('content');
+        // load markdown from github readme
+        $.ajax({
+            url: 'https://raw.githubusercontent.com/anuvgupta/sitarhero/master/README.md',
+            success: function (data) {
+                // resize window handler
+                $(window).resize(function () {
+                    var screenshot = $("img[alt='Sitar Hero Screenshot']");
+                    if (window.innerWidth < 800)
+                        content.css({
+                            width: '92.6%',
+                            borderRadius: '5px',
+                            margin: '40px auto 0 auto'
+                        });
+                    else content.css({
+                        width: '75%',
+                        borderRadius: '10px',
+                        margin: '40px auto'
                     });
-                else content.css({
-                    width: '75%',
-                    borderRadius: '10px',
-                    margin: '40px auto'
+                    if (window.innerWidth < 550)
+                        screenshot.css('width', '95%');
+                    else screenshot.css('width', 'auto');
                 });
-                if (window.innerWidth < 550)
-                    screenshot.css('width', '95%');
-                else screenshot.css('width', 'auto');
-            });
-            var renderer = new marked.Renderer();
-            content.html(marked(data, { renderer: renderer })).css('opacity', '1');
-            sitarhero.css('opacity', '1');
-            $(window).resize();
-            var index = window.location.href.indexOf('#');
-            if (index != -1) {
-                var id = window.location.href.substring(index + 1);
-                if (document.getElementById(id) != null) {
-                    $(document.body).animate({
-                        scrollTop: $('#' + id).offset().top + 'px'
-                    }, /* 700 */ 0);
+                var renderer = new marked.Renderer();
+                content.html(marked(data, { renderer: renderer })).css('opacity', '1');
+                sitarhero.fill(document.body);
+                document.body.style.opacity = '1';
+                $(window).resize();
+                var index = window.location.href.indexOf('#');
+                if (index != -1) {
+                    var id = window.location.href.substring(index + 1);
+                    if (document.getElementById(id) != null) {
+                        $(document.body).animate({
+                            scrollTop: $('#' + id).offset().top + 'px'
+                        }, /* 700 */ 0);
+                    }
                 }
             }
-        }
-    });
+        });
+    }, 'app', 'jQuery');
 });
