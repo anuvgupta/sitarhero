3fc8149ff76365337a17ec978ca57f8550ffc02f
Anuv Gupta <anuv.ca@gmail.com>
2016-09-18 03:06:14 -0700

    embellish

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
+    var sitarhero = $('#SitarHero');
     // resize window handler
     $(window).resize(function () {
         if (window.innerWidth < 800)
-            $('#main').css('width', '90%');
-        else $('#main').css('width', '70%');
+            sitarhero.css('width', '90%');
+        else sitarhero.css('width', '70%');
     });
     // load markdown from github readme
     $.ajax({
         url: 'https://raw.githubusercontent.com/anuvgupta/sitarhero/master/README.md',
         success: function (data) {
-            $('#main').html(marked(data));
+            sitarhero.html(marked(data)).css('opacity', '1');
             $(window).resize();
         }
     });
