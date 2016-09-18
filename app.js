fe7941b3c6e7e6ff4b885b741baa815ee5bea3a5
Anuv Gupta <anuv.ca@gmail.com>
2016-09-18 15:08:01 -0700

    removed old stuff

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 $(document).ready(function () {
-    var heading = $('#heading');
-    var A = $('#heading #A');
-    var B = $('#heading #B');
-    var author = $('#heading #B');
-    var screenshot = $('#screenshot');
     var sitarhero = $('#SitarHero');
     // resize window handler
     $(window).resize(function () {
         // resize content
         if (window.innerWidth < 800) {
-            screenshot.css('margin-right', '2%')
-                .css('margin-bottom', '-125px');
             sitarhero.css('width', '90%');
         } else {
-            screenshot.css('margin-right', '5%')
-                .css('margin-bottom', '-30%');
             sitarhero.css('width', '75%');
         }
-        // resize heading
-        var imgHeight = screenshot.height();
-        heading.css('height', imgHeight + 'px');
-        var fontsize = 100;
-        if (imgHeight < 250) fontsize = 60;
-        else if (imgHeight < 320) fontsize = 80;
-        A.css('font-size', fontsize + 'px');
-        B.css('font-size', fontsize * (3/5) + 'px');
     });
     // load markdown from github readme
     $.ajax({
